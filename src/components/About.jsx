ca8f76fcfa5a80ea2959e70ac831908c645125cb
waseemzahid <34806507+waseemzahid@users.noreply.github.com>
2025-04-15 12:22:07 +0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,7 +19,7 @@ const About = () => {
           </span>
         </h3>
         <p className="text-gray-400 my-3 text-lg">My introduction</p>
-        <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
+        <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-8xl mx-auto">
           <div className="p-2">
             <div className="text-gray-300 my-3">
               <ul className="text-base lg:text-lg list-none text-left">
